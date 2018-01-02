978735d76a90ffb2ef56f10a1e49887837b9a1df
callum-lawson <callumrlawson@gmail.com>
2018-01-02 17:19:54 +0000

    Include qsub arguments within submit script.

--- Source/invasion_submit.sh
+++ Source/invasion_submit.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
  
+#$ -S /bin/bash 
+#$ -N invasion_submit 
+#$ -l h_rt=60,h_vmem=1G 
+#$ -binding linear:1 
+
+# Output files 
+#$ -o invasion.out 
+#$ -e invasion.err 
+ 
+ # -o /work/$USER/$JOB_NAME-$JOB_ID
+ 
+# Bash script
 for arg in "$@" ; do
   echo $arg
 done
+
+# Run script: bash invasion_submit.sh 1 2 3 4
